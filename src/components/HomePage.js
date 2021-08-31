6ea8a7b97a16fcbd1ee2626a320d8f2aa4a50adc
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-08-31 23:50:31 +0900

    build a react function component

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <div className="jumbotron">
+            <h1>React App</h1>
+            <p>React, React Router, Redux, React Bootstrap, etc...</p>
+        </div>
+    );
+}
+
+export default HomePage;
