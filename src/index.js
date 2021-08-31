6a392751a33329aef29c68349d67c18b567afd87
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-09-01 01:34:14 +0900

    rendering HomePage.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import HomePage from "./components/HomePage";
+
+ReactDOM.render(<HomePage />, document.getElementById("root"));
