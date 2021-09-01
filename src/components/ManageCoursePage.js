4f9f6abdae0d7a71c3a2d5e067d67db868f44001
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-09-02 03:36:32 +0900

    Add CourseForm.js layout

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Prompt } from "react-router-dom";
+import CourseForm from "./CourseForm";
+// import { Prompt } from "react-router-dom";
 
 const ManageCoursePage = (props) => {
     return (
         <>
             <h2>Manage Course</h2>
-            <Prompt when={true} message="Are you sure you want to leave?" />
+            <CourseForm />
+            {/* <Prompt when={true} message="Are you sure you want to leave?" /> */}
             {props.match.params.slug}
         </>
     );
