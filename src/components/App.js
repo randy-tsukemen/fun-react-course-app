93d29d2093396cd5c54b0c53506c29bc4d91d69e
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-09-01 22:16:24 +0900

    Add CoursesPage and Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import CoursesPage from "./CoursesPage";
+import Header from "./common/Header";
 
 function App() {
-    const route = window.location.pathname;
-    if (route === "/") {
-        return <HomePage />;
-    } else if (route === "/about") {
-        return <AboutPage />;
+    function getPage() {
+        switch (window.location.pathname) {
+            case "/":
+                return <HomePage />;
+            case "/about":
+                return <AboutPage />;
+            case "/courses":
+                return <CoursesPage />;
+            default:
+                return null;
+        }
     }
+    return (
+        <div className="container-fluid">
+            <Header />
+            {getPage()}
+        </div>
+    );
 }
 
 export default App;
