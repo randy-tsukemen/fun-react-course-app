30cbdc6291ab77598fe1e6ed8938606959dbcd72
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-09-02 00:24:53 +0900

    Add client side Link

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
     return (
         <div className="jumbotron">
             <h1>React App</h1>
             <p>React, React Router, Redux, React Bootstrap, etc...</p>
-            <a href="/about">About</a>
+            {/* <a href="/about">About</a> */}
+            <Link to="/about" className="btn btn-primary btn-lg">
+                About
+            </Link>
         </div>
     );
 }
